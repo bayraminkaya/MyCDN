60d14d3c50ef85dea04788f58611e11610353ff7
bayraminkaya <91468628+bayraminkaya@users.noreply.github.com>
2024-02-13 11:08:02 +0300

    Add files via upload

---
+++ Application/Services/IUserService.cs
@@ -0,0 +1,16 @@
+﻿using MyCDN.WebApi.Application.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyCDN.WebApi.Application.Services
+{
+    public interface IUserService
+    {
+        Task<IEnumerable<UserDTO>> GetAllUsersAsync();
+        Task<UserDTO> GetUserByIdAsync(int userId);
+        
+    }
+}
