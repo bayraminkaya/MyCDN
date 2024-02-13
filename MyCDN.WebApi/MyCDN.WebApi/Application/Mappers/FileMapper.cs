1a90337c873e075c93f66b71ad4f6152cf9c22bb
bayraminkaya <91468628+bayraminkaya@users.noreply.github.com>
2024-02-13 11:16:24 +0300

    Add files via upload

---
+++ MyCDN.WebApi/MyCDN.WebApi/Application/Mappers/FileMapper.cs
@@ -0,0 +1,24 @@
+﻿using MyCDN.WebApi.Application.DTOs;
+using MyCDN.WebApi.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyCDN.WebApi.Application.Mappers
+{
+    public static class FileMapper
+    {
+        public static FileDTO ToDTO(this MyFile file)
+        {
+            return new FileDTO
+            {
+                Id = file.Id,
+                FileName = file.FileName,
+                Password = file.Password, 
+                
+            };
+        }
+    }
+}
---
+++ MyCDN.WebApi/MyCDN.WebApi/Application/Services/IFileService.cs
@@ -0,0 +1,16 @@
+﻿using MyCDN.WebApi.Application.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyCDN.WebApi.Application.Services
+{
+    public interface IFileService
+    {
+        Task<IEnumerable<FileDTO>> GetAllFilesAsync();
+        Task<FileDTO> GetFileByIdAsync(int fileId);
+        
+    }
+}
